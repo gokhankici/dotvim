7eb237749ef3965c5aa32b1b2dbbdaff9dc0fc48
Rami Gökhan Kıcı <gokhankici@gmail.com>
2013-06-12 15:31:19 +0300

    Xmodmap added

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+ln -s ~/.vim/vimrc ~/.vimrc
+ln -s ~/.vim/gvimrc ~/.gvimrc
+ln -s ~/.vim/tmux.conf ~/.tmux.conf
+ln -s ~/.vim/Xresources ~/.Xresources
+ln -s ~/.vim/Xmodmap ~/.Xmodmap
+ln -s ~/.vim/screenrc ~/.screenrc
